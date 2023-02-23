eebf9b48a978d56c49b1ed791849fe9966e2a354
noreenh21 <hossain.n@northeastern.edu>
2023-02-23 15:22:26 -0500

    petal vs sepal length plot

---
+++ main.js
@@ -0,0 +1,84 @@
+const FRAME_HEIGHT = 400;
+const FRAME_WIDTH = 400;
+const MARGINS = {left: 30, right: 20, top: 20, bottom: 20};
+
+function map_color(species){
+    if(species == "setosa"){
+        return "coral";
+    }
+    if(species == "versicolor"){
+        return "orchid";
+    }
+    if(species == "virginica"){
+        return "cornflowerblue";
+    }
+    else{
+        return black;
+    }
+};
+
+// ------------ LEFT PLOT -----------------
+// Make svg to house visualization
+const LEFT = d3.select("#left") 
+.append("svg") 
+    .attr("height", FRAME_HEIGHT)   
+    .attr("width", FRAME_WIDTH)
+    .attr("class", "frame");
+
+d3.csv("data/iris.csv").then((data) => {
+    // console.log(data);
+    const X_MAX = d3.max(data, (d) => {return (d.Sepal_Length)});
+    const Y_MAX = d3.max(data, (d) => { return (d.Petal_Length); });
+    const X_MIN = d3.min(data, (d) => {return (d.Petal_Length)});
+    const Y_MIN = d3.min(data, (d) => { return (d.Sepal_Length); });
+    console.log(X_MIN, Y_MIN);
+
+    // TITLE
+    LEFT.append("text")
+    .attr("x", FRAME_WIDTH/2)
+    .attr("y", MARGINS.top)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .text("Sepal Length vs Petal Length");
+
+    // Make x-axis based on petal length
+    let x = d3.scaleLinear()
+    .domain([0, parseInt(X_MAX) + 1])
+    .range([ 0, FRAME_WIDTH - MARGINS.left - MARGINS.right]);
+    // Add X-axis
+    LEFT.append("g")
+    .attr("transform", "translate(" +  MARGINS.left +","+ (FRAME_HEIGHT - MARGINS.top) + ")")
+    .call(d3.axisBottom(x));
+
+    // y-Scale
+    let y = d3.scaleLinear()
+    .domain([0, parseInt(Y_MAX) + 1])
+    .range([FRAME_HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+    // Add Y axis
+    LEFT.append("g")
+    .attr("transform", "translate(" + MARGINS.left + ","+ MARGINS.top +")")
+    .call(d3.axisLeft(y));
+
+     // Add points from data
+     LEFT.append('g')
+     .selectAll("dot")
+     .data(data)
+     .enter()
+     .append("circle")
+         .attr("class", "point")
+         .attr("id", (d) => {return d.id})
+         .attr("cx", (d) => { return (x(d.Sepal_Length) + MARGINS.left); } )
+         .attr("cy", (d) =>{ return (y(d.Petal_Length) + MARGINS.top); } )
+         .style("fill", (d) => {return map_color(d.Species)})
+         .attr("r", 3);
+
+});
+
+// -------------------- MIDDLE PLOT -------------------------
+const MIDDLE = d3.select("#middle") 
+.append("svg") 
+    .attr("height", FRAME_HEIGHT)   
+    .attr("width", FRAME_WIDTH)
+    .attr("class", "frame");
+
+d3.csv("data/iris.csv").then((data) => {});
