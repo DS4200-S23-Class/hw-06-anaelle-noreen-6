a7dca5ec86ba59fff5d46b42a5be7ff81e7f5b2b
noreenh21 <hossain.n@northeastern.edu>
2023-02-23 15:36:37 -0500

    Sepal vs Petal Width

--- main.js
+++ main.js
@@ -29,9 +29,6 @@ d3.csv("data/iris.csv").then((data) => {
     // console.log(data);
     const X_MAX = d3.max(data, (d) => {return (d.Sepal_Length)});
     const Y_MAX = d3.max(data, (d) => { return (d.Petal_Length); });
-    const X_MIN = d3.min(data, (d) => {return (d.Petal_Length)});
-    const Y_MIN = d3.min(data, (d) => { return (d.Sepal_Length); });
-    console.log(X_MIN, Y_MIN);
 
     // TITLE
     LEFT.append("text")
@@ -81,4 +78,50 @@ const MIDDLE = d3.select("#middle")
     .attr("width", FRAME_WIDTH)
     .attr("class", "frame");
 
-d3.csv("data/iris.csv").then((data) => {});
+d3.csv("data/iris.csv").then((data) => {
+    const X_MAX = d3.max(data, (d) => {return (d.Sepal_Width)});
+    const Y_MAX = d3.max(data, (d) => { return (d.Petal_Width); });
+    console.log("SEPAL" + X_MAX, "PETAL" + Y_MAX);
+
+    // TITLE
+    MIDDLE.append("text")
+    .attr("x", FRAME_WIDTH/2)
+    .attr("y", MARGINS.top)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px")
+    .text("Sepal Width vs Petal Width");
+
+    // Make x-axis based on petal width
+    let x = d3.scaleLinear()
+    .domain([0, parseInt(X_MAX) + 1])
+    .range([ 0, FRAME_WIDTH - MARGINS.left - MARGINS.right]);
+
+    // Add X-axis
+    MIDDLE.append("g")
+    .attr("transform", "translate(" +  MARGINS.left +","+ (FRAME_HEIGHT - MARGINS.top) + ")")
+    .call(d3.axisBottom(x));
+
+    // y-Scale
+    let y = d3.scaleLinear()
+    .domain([0, parseInt(Y_MAX) + 1])
+    .range([FRAME_HEIGHT - MARGINS.top - MARGINS.bottom, 0]);
+    // Add Y axis
+    MIDDLE.append("g")
+    .attr("transform", "translate(" + MARGINS.left + ","+ MARGINS.top +")")
+    .call(d3.axisLeft(y));
+
+    // Add points from data
+    MIDDLE.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("class", "point")
+        .attr("id", (d) => {return d.id})
+        .attr("cx", (d) => { return (x(d.Sepal_Width)) + MARGINS.left; } )
+        .attr("cy", (d) =>{ return (y(d.Petal_Width)) + MARGINS.bottom; } )
+        .style("fill", (d) => {return map_color(d.Species)})
+        .attr("r", 3);
+
+
+});
