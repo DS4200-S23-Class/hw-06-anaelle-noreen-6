9ca57c421344e71e502da4f71bb35bc20fcd4741
noreenh21 <hossain.n@northeastern.edu>
2023-03-01 10:09:40 -0500

    Final linking works

--- main.js
+++ main.js
@@ -18,7 +18,9 @@ function map_color(species){
     }
 };
 
+// declare the plotted dots and bars
 let left_dots, middle_dots, bars;
+// declare the x and y scaling functions for each plot
 let left_x, middle_x, left_y, middle_y, bars_x, bars_y;
 
 // ------------ LEFT PLOT -----------------
@@ -34,15 +36,7 @@ d3.csv("data/iris.csv").then((data) => {
     const X_MAX = d3.max(data, (d) => {return (d.Sepal_Length)});
     const Y_MAX = d3.max(data, (d) => { return (d.Petal_Length); });
 
-    // // TITLE
-    // LEFT.append("text")
-    // .attr("x", FRAME_WIDTH/2)
-    // .attr("y", MARGINS.top)
-    // .attr("class", "plot_titles")
-    // .attr("text-anchor", "middle")
-    // .style("font-size", "16px")
-    // .text("Sepal Length vs Petal Length");
-
+   
     // Make x-axis based on petal length
     left_x = d3.scaleLinear()
     .domain([0, parseInt(X_MAX) + 1])
@@ -67,7 +61,7 @@ d3.csv("data/iris.csv").then((data) => {
      .data(data)
      .enter()
      .append("circle")
-         .attr("class",  (d) => {return "point " + d.Species})
+         .attr("class",  (d) => {return d.Species})
          .attr("id", (d) => {return d.id})
          .attr("cx", (d) => { return (left_x(d.Sepal_Length) + MARGINS.left); } )
          .attr("cy", (d) =>{ return (left_y(d.Petal_Length) + MARGINS.top); } )
@@ -78,7 +72,7 @@ d3.csv("data/iris.csv").then((data) => {
 
 // -------------------- MIDDLE PLOT -------------------------
 const MIDDLE = d3.select("#middle") 
-.append("svg") 
+    .append("svg") 
     .attr("height", FRAME_HEIGHT)   
     .attr("width", FRAME_WIDTH)
     .attr("class", "frame");
@@ -87,15 +81,6 @@ d3.csv("data/iris.csv").then((data) => {
     const X_MAX = d3.max(data, (d) => {return (d.Sepal_Width)});
     const Y_MAX = d3.max(data, (d) => { return (d.Petal_Width); });
     console.log("SEPAL" + X_MAX, "PETAL" + Y_MAX);
-
-    // TITLE
-    // MIDDLE.append("text")
-    // .attr("x", FRAME_WIDTH/2)
-    // .attr("y", MARGINS.top)
-    // .attr("class", "plot_titles")
-    // .attr("text-anchor", "middle")
-    // .style("font-size", "16px")
-    // .text("Sepal Width vs Petal Width");
 
     // Make x-axis based on petal width
     middle_x = d3.scaleLinear()
@@ -122,7 +107,7 @@ d3.csv("data/iris.csv").then((data) => {
     .data(data)
     .enter()
     .append("circle")
-        .attr("class", (d) => {return "point " + d.Species})
+        .attr("class", (d) => {return d.Species})
         .attr("id", (d) => {return d.id })
         .attr("cx", (d) => { return (middle_x(d.Sepal_Width)) + MARGINS.left; } )
         .attr("cy", (d) =>{ return (middle_y(d.Petal_Width)) + MARGINS.bottom; } )
@@ -130,8 +115,7 @@ d3.csv("data/iris.csv").then((data) => {
         .attr("r", 5);
 
 
-    
-
+  
 // ------------------ RIGHT PLOT -----------------------------------------
 // Add the SVG
 const RIGHT = d3.select("#right") 
@@ -143,74 +127,61 @@ const RIGHT = d3.select("#right")
 
 d3.csv("data/iris.csv").then((data) => {
 
-    // Makes a Map with the species name, and the number of times that species appears in the column
-    let species_counts = Array.from(d3.rollup(data, v => v.length, d => d.Species));
-    let species_amt = species_counts.map(function(x){
-        return x[1];
-    });
-
-    // Gets the hightest amount
-    const Y_MAX = d3.max(species_amt);
-
-    // // TITLE
-    // RIGHT.append("text")
-    // .attr("x", FRAME_WIDTH/2)
-    // .attr("y", MARGINS.top)
-    // .attr("class", "plot_titles")
-    // .attr("text-anchor", "middle")
-    // .style("font-size", "16px")
-    // .text("Counts of Species");
-
-
     // Add X axis
     bars_x = d3.scaleBand()
         .range([ 0, FRAME_WIDTH - MARGINS.left - MARGINS.right ])
-        .domain(species_counts.map(function(d) { return d[0]; }))
+        .domain(data.map(function(d){return d.Species;}))
         .padding(0.2);
-
     RIGHT.append("g")
-        .attr("transform", "translate(" +  MARGINS.left +","+ (FRAME_HEIGHT - MARGINS.top) + ")")
+        .attr("transform", "translate(" + MARGINS.left+","+ (FRAME_HEIGHT - MARGINS.top) + ")")
         .call(d3.axisBottom(bars_x))
         .selectAll("text")
         .style("text-anchor", "middle");
 
     // Add Y axis
     bars_y = d3.scaleLinear()
-        .domain([0, Y_MAX + 5])
+        .domain([0,55])
         .range([FRAME_HEIGHT- MARGINS.top - MARGINS.bottom, 0]);
     RIGHT.append("g")
-        .attr("transform", "translate(" +  MARGINS.left +","+(MARGINS.top)+ ")")
+        .attr("transform", "translate(" + MARGINS.left +","+ MARGINS.top + ")")
         .call(d3.axisLeft(bars_y));
+        
 
     // Add Bars
-    bars = RIGHT.selectAll("mybar")
-        .data(species_counts)
+    bars = RIGHT.append('g')
+        .selectAll("bars")
+        .data(data)
         .enter()
         .append("rect")
-        .attr("class", "bars")
-        .attr("x", function(d) { return bars_x(d[0]) + MARGINS.left; })
-        .attr("y", function(d) { return bars_y(d[1]) + MARGINS.top; })
-        .style("fill", (d) => {return map_color(d[0])})
+        .attr("class", (d) => {return d.Species})
+        .attr("id", (d) => {return d.id})
+        .attr("x",  (d) => {return bars_x(d.Species) + MARGINS.left})
+        .attr("y",  bars_y(50) + MARGINS.top)
         .attr("width", bars_x.bandwidth())
         .attr("height", function(d) { 
-            return FRAME_HEIGHT- MARGINS.top - MARGINS.bottom - bars_y(d[1]); 
+            return FRAME_HEIGHT- MARGINS.top - MARGINS.bottom - bars_y(50); 
+        })
+        .style("fill", (d) => {return map_color((d.Species))})
+        // for some reason this styling does not apply. It appears in teh inspector, but crossed out
+        .style("opacity", 0.5);
     });
-});
 
 // ---------------------- BRUSHING AND LINKING ------------------------------------------------
 // Function that is triggered when brushing is performed
 function updateCharts(event) {
+    // selection area
     extent = event.selection
-    let selection_points;
+    // lmiddle scatterplot dots that are brushed 
     middle_dots.classed("selected", function(d){ 
         return isBrushed(extent, (middle_x(d.Sepal_Width) + MARGINS.left), (middle_y(d.Petal_Width) + MARGINS.top)) 
     } );
+    // corresponding left plot dots when middle is brushed 
     left_dots.classed("selected", function(d){ 
         return isBrushed(extent, (middle_x(d.Sepal_Width) + MARGINS.left), (middle_y(d.Petal_Width) + MARGINS.top)) 
     } ); 
-    
+    // correspoingding bars when middle is brushed
     bars.classed("selected", function(d){ 
-        return isBrushed(extent, (bars_x(d.Sepal_Width) + MARGINS.left), (bars_y(d.Petal_Width) + MARGINS.top)) 
+        return isBrushed(extent, (middle_x(d.Sepal_Width) + MARGINS.left), (middle_y(d.Petal_Width) + MARGINS.top)) 
     });        
 };
 
